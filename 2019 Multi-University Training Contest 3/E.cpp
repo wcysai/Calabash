3812dc2490e237450e6406385336549802976a28
wcysai <wcysai@foxmail.com>
2019-07-29 20:11:44 +0800

    upsolved E

--- 2019 Multi-University Training Contest 3/E.cpp
+++ 2019 Multi-University Training Contest 3/E.cpp
@@ -12,14 +12,13 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> P;
 bool is_prime[MAXN];
-int cnt,mmiu[MAXN],prime[MAXN];
+int cnt,mphi[MAXN],prime[MAXN];
 int T;
 ll n,m;
 int k,f[MAXM],g[MAXM];
-int miuf[MAXN],miug[MAXM];
+int phif[MAXN];
 int pf[MAXM];
 unordered_map<ll,int> mp;
-unordered_map<ll,int> mp2;
 vector<int>poly;
 int dif[MAXM];
 int inv[MAXM];
@@ -54,13 +53,11 @@ void getpoly()
 }
 int getsum(ll x)
 {
-    if(x+1>=MOD) return 0;
-    int res=x+1,ans=0;
+    int res=(x+1)%MOD,ans=0;
     for(int i=0;i<=k;i++)
     {
         add(ans,1LL*res*poly[i]%MOD);
-        if(i>=x+1) break;
-        res=1LL*res*(x-i)%MOD;
+        res=1LL*res*((x-i)%MOD)%MOD;
         res=1LL*res*inv[i+2]%MOD;
     }
     return ans;
@@ -75,10 +72,10 @@ void genf(ll n)
         if(g[i]==g[i-1]) continue;
         for(j=1;j<=min(m-1,n/i/i);++j)
         {
-            if(i*j<m) dec(f[j],1LL*p[i]*(f[i*j]-g[i-1])%MOD),add(f[j],0);
-            else dec(f[j],p[i]*(g[n/i/j]-g[i-1])),add(f[j],0);
+            if(i*j<m) dec(f[j],1LL*p[i]*(f[i*j]-g[i-1]+MOD)%MOD);
+            else dec(f[j],1LL*p[i]*(g[n/i/j]-g[i-1]+MOD)%MOD);
         }
-        for(j=m;j>=i*i;j--) dec(g[j],1LL*p[i]*(g[j/i]-g[i-1])),add(g[j],0);
+        for(j=m;j>=i*i;j--) dec(g[j],1LL*p[i]*(g[j/i]-g[i-1]+MOD)%MOD);
     }
 }
 int getpr(ll x)
@@ -86,24 +83,24 @@ int getpr(ll x)
     if(x<=m) return g[x];
     return f[n/x];
 }
-void genmiu(int n)
+void gen(int n)
 {
     int p=0;
     for(int i=0;i<=n;i++) is_prime[i]=true;
     is_prime[0]=is_prime[1]=false;
-    mmiu[1]=1;
+    mphi[1]=1;
     for(int i=2;i<=n;i++)
     {
-        if(is_prime[i]) {prime[p++]=i; mmiu[i]=MOD-1LL*i*i%MOD;}
+        if(is_prime[i]) {prime[p++]=i; mphi[i]=1LL*i*i%MOD*(i-1)%MOD;}
         for(int j=0;j<p;j++)
         {
             if(prime[j]*i>n) break;
             is_prime[prime[j]*i]=false;
-            mmiu[i*prime[j]]=i%prime[j]?(MOD-1LL*mmiu[i]*prime[j]%MOD*prime[j]%MOD):0;
+            mphi[i*prime[j]]=i%prime[j]?(1LL*mphi[i]*mphi[prime[j]]%MOD):1LL*mphi[i]*prime[j]%MOD*prime[j]%MOD*prime[j]%MOD;
             if(i%prime[j]==0) break;
         }
     }
-    for(int i=1;i<=n;i++) miuf[i]=miuf[i-1],add(miuf[i],mmiu[i]);
+    for(int i=1;i<=n;i++) phif[i]=phif[i-1],add(phif[i],mphi[i]);
 }
 int getsum3(ll x)
 {
@@ -112,38 +109,34 @@ int getsum3(ll x)
     if(res1%3==0) res1/=3; else if(res2%3==0) res2/=3; else res3/=3;
     return 1LL*(res1%MOD)*(res2%MOD)%MOD*(res3%MOD)%MOD;
 }
+int getsum4(ll x)
+{
+    if(x&1)
+    {
+        int tmp1=((x+1)/2)%MOD,tmp2=x%MOD;
+        return 1LL*tmp2*tmp2%MOD*tmp1%MOD*tmp1%MOD;
+    }
+    else
+    {
+        int tmp1=(x/2)%MOD,tmp2=(x+1)%MOD;
+        return 1LL*tmp2*tmp2%MOD*tmp1%MOD*tmp1%MOD;
+    }
+}
 int calc(ll x)
 {
-	if(x<=10000000) return miuf[x];
+	if(x<=10000000) return phif[x];
 	if(mp.find(x)!=mp.end()) return mp[x];
-	int ans=1;
+	int ans=getsum4(x);
 	for(ll i=2,r;i<=x;i=r+1)
 	{
 		r=x/(x/i);
-		dec(ans,1LL*calc(x/i)*(getsum3(r)-getsum3(i-1))%MOD);
+		dec(ans,1LL*calc(x/i)*(getsum3(r)-getsum3(i-1)+MOD)%MOD);
 	}
 	return mp[x]=ans;
 }
-int getff(ll x)
-{
-    if(mp2.find(x)!=mp2.end()) return mp2[x];
-    int ans=0;
-    for(ll i=1,r;i<=x;i=r+1)
-    {
-        r=x/(x/i);
-        int res=calc(r);
-        dec(res,calc(i-1));
-        int tmp=(x/i)%MOD;
-        res=1LL*res*tmp%MOD*tmp%MOD;
-        res=1LL*res*(tmp+1)%MOD*(tmp+1)%MOD;
-        res=1LL*res*INV%MOD;
-        add(ans,res);
-    }
-    return ans%MOD;
-}
 int main()
 {
-	genmiu(10000000);
+	gen(10000000);
 	for(int i=1;i<=100002;i++) inv[i]=pow_mod(i,MOD-2);
 	scanf("%d",&T);
 	while(T--)
@@ -151,7 +144,6 @@ int main()
         scanf("%lld%d",&n,&k);
         for(m=1;1LL*m*m<=n;m++);
         k++;
-        p[0]=1;
         for(int i=1;i<=m;i++)
         {
             p[i]=1;
@@ -166,7 +158,7 @@ int main()
             r=n/(n/i);
             int res=getpr(r);
             dec(res,getpr(i-1));
-            res=1LL*res*getff(n/i)%MOD;
+            res=1LL*res*calc(n/i)%MOD;
             add(ans,res);
         }
         printf("%d\n",ans);
